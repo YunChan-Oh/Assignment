fdf17bb03cbc1b5e7b2176ab0f41aefc141f2dc3
Yunchan-Oh <oyunchan9@gmail.com>
2025-06-09 14:20:43 +0900

    init

---
+++ src/main/kotlin/com/sionicai/assignment/SionicaiApplication.kt
@@ -0,0 +1,11 @@
+package com.sionicai.assignment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SionicaiApplication
+
+fun main(args: Array<String>) {
+    runApplication<SionicaiApplication>(*args)
+}
---
+++ src/test/kotlin/com/sionicai/assignment/SionicaiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sionicai.assignment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SionicaiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
